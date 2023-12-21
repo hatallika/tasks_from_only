ac3979dac3838c15ebec659e12b5a80a31f62223
hatallika <essenadmin@gmail.com>
2023-12-21 22:39:42 +0300

    added module with complex property

---
+++ local/modules/phpdevorg.cprop/include.php
@@ -0,0 +1,6 @@
+<?php
+
+\Bitrix\Main\Loader::registerAutoLoadClasses('phpdevorg.cprop', [
+    'CIBlockPropertyCProp' => 'lib/CIBlockPropertyCProp.php',
+    'CCustomTypeHtml' => 'lib/CCustomTypeHtml.php',
+]);
